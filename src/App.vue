e21bd327b7cf3c1c1418a9b0d28602229d622ff5
Alexei <leshkapryanik@gmail.com>
2019-12-14 14:57:27 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import usercard from '.components/usercard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    usercard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
